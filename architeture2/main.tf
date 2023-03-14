d214f91051b4505d14868a33bce4034ead9b5ddd
ImagineBrkr <salvattore_25@hotmail.com>
2023-03-14 12:44:15 -0500

    test

--- architeture2/main.tf
+++ architeture2/main.tf
@@ -2,34 +2,33 @@ provider "aws" {
   region      = "us-east-1"
 }
 
-# resource "aws_lambda_function" "hello-terraform" {
-#     filename = "${local.building_path}/${local.lambda_code_filename}"
-#     handler = "lambda_function.lambda_handler"
-#     runtime = "python3.9"
-#     function_name = "hello_function"
-#     role = aws_iam_role.iam_for_lambda.arn
-#     timeout = 30
-# }
+resource "aws_lambda_function" "hello_function" {
+    filename = "${local.building_path}/${local.lambda_code_filename}"
+    handler = "lambda_function.lambda_handler"
+    runtime = "python3.9"
+    function_name = "hello_function"
+    role = aws_iam_role.iam_for_lambda.arn
+    timeout = 30
+}
 
 //Creates lambda function
-module "hello_function" {
-  source        = "terraform-aws-modules/lambda/aws"
-  version       = "4.6.0"
-  create_role   = false 
-  timeout       = 30
-  create_package         = false
-  local_existing_package = "${locals.building_path}/${locals.lambda_code_filename}"
-  function_name = "hello_function"
-  handler       = "lambda_function.lambda_handler"
-  runtime       = "python3.9"
-  lambda_role   = aws_iam_role.iam_for_lambda.arn
-  attach_cloudwatch_logs_policy = false
-  attach_dead_letter_policy     = false
-  attach_network_policy         = false
-  attach_tracing_policy         = false
-  attach_async_event_policy     = false
-
-}
+# module "hello_function" {
+#   source        = "terraform-aws-modules/lambda/aws"
+#   version       = "4.6.0"
+#   create_role   = false 
+#   timeout       = 30
+#   create_package         = false
+#   local_existing_package = "${local.building_path}/${local.lambda_code_filename}"
+#   function_name = "hello_function"
+#   handler       = "lambda_function.lambda_handler"
+#   runtime       = "python3.9"
+#   lambda_role   = aws_iam_role.iam_for_lambda.arn
+#   attach_cloudwatch_logs_policy = false
+#   attach_dead_letter_policy     = false
+#   attach_network_policy         = false
+#   attach_tracing_policy         = false
+#   attach_async_event_policy     = false
+# }
 
 //IAM Role policy for lambda function
 resource "aws_iam_role" "iam_for_lambda" {
@@ -70,7 +69,7 @@ resource "aws_apigatewayv2_stage" "lambda" {
 resource "aws_apigatewayv2_integration" "get_hello_function" {
   api_id = aws_apigatewayv2_api.lambda.id
 
-  integration_uri    = module.hello_function.lambda_function_invoke_arn
+  integration_uri    = aws_lambda_function.hello_function.invoke_arn
   integration_type   = "AWS_PROXY"
   integration_method = "POST"
 }
@@ -87,7 +86,7 @@ resource "aws_apigatewayv2_route" "hello_function" {
 resource "aws_lambda_permission" "api_gw" {
   statement_id  = "AllowExecutionFromAPIGateway"
   action        = "lambda:InvokeFunction"
-  function_name = module.hello_function.lambda_function_name
+  function_name = aws_lambda_function.hello_function.function_name
   principal     = "apigateway.amazonaws.com"
 
   source_arn = "${aws_apigatewayv2_api.lambda.execution_arn}/*/*"
