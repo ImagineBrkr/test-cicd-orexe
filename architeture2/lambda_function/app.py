931094914a5b1da50a7ebf75cfbb452003d3a4ec
ImagineBrkr <salvattore_25@hotmail.com>
2023-03-14 11:47:15 -0500

    test

--- architeture2/lambda_function/app.py
+++ architeture2/lambda_function/app.py
@@ -7,6 +7,5 @@ def lambda_handler(event, context):
         "statusCode": 200,
         "body": json.dumps({
             "message": "hello world",
-            # "location": ip.text.replace("\n", "")
         }),
     }
---
+++ architeture2/main.tf
@@ -0,0 +1,81 @@
+provider "aws" {
+  region      = "us-east-1"
+  access_key  = "AKIATCN5HED6S3BM74F6" //To be replaced
+  secret_key  = "RCUaUR0Ney65zYR/rWWnfpEnejaHSK3WUvWOrUYn" //To be replaced
+}
+
+
+//Creates lambda function
+module "hello_function" {
+  source        = "terraform-aws-modules/lambda/aws"
+  version       = "4.6.0"
+  create_role   = false 
+  timeout       = 30
+  source_path   = local.lambda_src_path
+  function_name = "hello_function"
+  handler       = "lambda_function.lambda_handler"
+  runtime       = "python3.9"
+  lambda_role   = aws_iam_role.iam_for_lambda.arn
+}
+
+//IAM Role policy for lambda function
+resource "aws_iam_role" "iam_for_lambda" {
+  name = "iam_for_lambda_usage"
+
+  assume_role_policy = <<EOF
+    {
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+        "Action": "sts:AssumeRole",
+        "Principal": {
+            "Service": "lambda.amazonaws.com"
+        },
+        "Effect": "Allow",
+        "Sid": ""
+        }
+    ]
+    }
+    EOF
+}
+
+//Creating api gateway
+resource "aws_apigatewayv2_api" "lambda" {
+  name          = "lambda_api_service"
+  protocol_type = "HTTP"
+}
+
+//Creating stage
+resource "aws_apigatewayv2_stage" "lambda" {
+  api_id = aws_apigatewayv2_api.lambda.id
+  name        = "prod"
+  auto_deploy = true
+
+}
+
+//Integrating lambda function with api
+resource "aws_apigatewayv2_integration" "get_hello_function" {
+  api_id = aws_apigatewayv2_api.lambda.id
+
+  integration_uri    = module.hello_function.lambda_function_invoke_arn
+  integration_type   = "AWS_PROXY"
+  integration_method = "GET"
+}
+
+//Route
+resource "aws_apigatewayv2_route" "hello_function" {
+  api_id = aws_apigatewayv2_api.lambda.id
+
+  route_key = "GET /hello"
+  target    = "integrations/${aws_apigatewayv2_integration.get_hello_function.id}"
+}
+
+//Giving permission for invoking lambda function
+resource "aws_lambda_permission" "api_gw" {
+  statement_id  = "AllowExecutionFromAPIGateway"
+  action        = "lambda:InvokeFunction"
+  function_name = module.hello_function.lambda_function_name
+  principal     = "apigateway.amazonaws.com"
+
+  source_arn = "${aws_apigatewayv2_api.lambda.execution_arn}/*/*"
+}
