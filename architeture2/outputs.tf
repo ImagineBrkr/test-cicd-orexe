11d65a70f16f571fd2864cff051b6dd6150ad30e
ImagineBrkr <salvattore_25@hotmail.com>
2023-03-14 12:15:37 -0500

    test

--- architeture2/outputs.tf
+++ architeture2/outputs.tf
@@ -3,7 +3,7 @@ output "lambda_arn" {
   value       = aws_apigatewayv2_stage.lambda.invoke_url
 }
 
-output "publish_book_url" {
+output "api_url" {
   description = "Deployment invoke url"
-  value     = "${aws_apigatewayv2_stage.lambda.invoke_url}/book-review"
+  value     = "${aws_apigatewayv2_stage.lambda.invoke_url}/hello"
 }
